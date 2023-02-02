1ed4f2ea004671650337fcc05a72643a5e5b0de1
manaramgadd <109552152+manaramgadd@users.noreply.github.com>
2023-02-02 05:20:28 +0200

    Add files via upload

---
+++ Webapi/Controllers/orderController.cs
@@ -0,0 +1,36 @@
+﻿using Cafeteria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cafeteria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public AppDb Db { get; }
+
+        public OrderController(AppDb db)
+        {
+            Db = db;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Order order)
+        {
+            await Db.Connection.OpenAsync();
+            order.Db = Db;
+            try
+            {
+                await order.Insert();
+                return new OkObjectResult(order);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Order cannot happen");
+            }
+        }
+    }
+}
