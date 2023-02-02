2fc18b4daba9dfc0a9a495157443e9d813a7394f
manaramgadd <109552152+manaramgadd@users.noreply.github.com>
2023-02-02 05:26:20 +0200

    Add files via upload

---
+++ FrontEnd/Helpers/Api.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FrontEndCafeteria.Helpers
+{
+    public class Api
+    {
+        public HttpClient Initial()
+        {
+            string baseUrl = "https://localhost:44370/";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+}
